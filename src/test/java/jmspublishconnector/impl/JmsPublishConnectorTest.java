adf2e4a1c7dc44254ae1ffceb78b2cf6a2a6e63b
MichalStefaniuk <michal.stefaniuk@devapo.pl>
2021-02-25 00:01:36 +0100

    AMRO-1 Cleanup tests code. Update target JAR

--- src/test/java/jmspublishconnector/impl/JmsPublishConnectorTest.java
+++ src/test/java/jmspublishconnector/impl/JmsPublishConnectorTest.java
@@ -22,8 +22,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class JmsPublishConnectorTest {
 
-    //@Rule
-    //public EmbeddedJMSResource broker = new EmbeddedJMSResource();
     BrokerService broker;
 
     protected JmsPublishConnector connector;
@@ -37,22 +35,22 @@ public class JmsPublishConnectorTest {
     }
 
     @Before
-    public void setupAMQ() throws Exception {
-
+    public void setupAMQ() throws Exception{
         broker = new BrokerService();
         TransportConnector connector = new TransportConnector();
         connector.setUri(new URI("tcp://localhost:61610"));
         broker.addConnector(connector);
         broker.setDeleteAllMessagesOnStartup(true);
-        // More config here!
         broker.start();
     }
-    public QueueViewMBean getProxyToQueue(String name) throws MalformedObjectNameException, JMSException {
+
+    public QueueViewMBean getProxyToQueue(String name) throws MalformedObjectNameException, JMSException{
         ObjectName queueViewMBeanName = new ObjectName("org.apache.activemq:type=Broker,brokerName=localhost,destinationType=Queue,destinationName="+name);
         QueueViewMBean proxy = (QueueViewMBean) broker.getManagementContext()
                 .newProxyInstance(queueViewMBeanName, QueueViewMBean.class, true);
         return proxy;
     }
+
     @Test
     public void shouldDiscoverConnector(){
         Connector jmsPublisher = Connectors.getConnector(JmsPublishConnector.ID);
@@ -61,40 +59,23 @@ public class JmsPublishConnectorTest {
 
     @Test
     public void connectorSendsMessageToJms() throws IOException, MalformedObjectNameException, JMSException {
+
         JmsRequest request = new JmsRequestImpl(connector);
+
         request.setRequestParameter("url", "tcp://localhost:61610");
         request.setRequestParameter("queue", "test1");
         request.setRequestParameter("message", "test");
 
         connector.execute(request);
         broker.checkQueueSize("test1");
+
         QueueViewMBean queueViewMBean = getProxyToQueue("test1");
-        System.out.println("123123123xxx" + queueViewMBean.getQueueSize());
+
+        assertThat(queueViewMBean.getQueueSize() == 1);
     }
 
     @After
     public void tearDownAMQ() throws Exception {
         broker.stop();
     }
-
-   /* @Test
-    public void connectorSendsMessageToJMS() throws JMSException {
-        final ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost?broker.persistent=false");
-        final javax.jms.Connection connection = connectionFactory.createConnection();
-        //connection.setClientID("12345");
-        connection.start();
-
-        final Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-        final Topic temporaryTopic = session.createTemporaryTopic();
-
-        final MessageConsumer consumer1 = session.createConsumer(temporaryTopic);
-
-        final MessageProducer producer = session.createProducer(temporaryTopic);
-        producer.send(session.createTextMessage("Testmessage"));
-
-        final TextMessage message = (TextMessage)consumer1.receiveNoWait();
-
-        Assert.assertNotNull(message);
-        Assert.assertEquals("Testmessage", message.getText());
-    }*/
 }
