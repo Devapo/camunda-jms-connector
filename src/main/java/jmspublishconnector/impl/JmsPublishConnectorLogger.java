96900f327a25b52fdd7dc2e553d672739109af03
MichalStefaniuk <michal.stefaniuk@devapo.pl>
2021-03-03 15:51:39 +0100

    AMRO-1 Bug fix. Logger extended wrong class

--- src/main/java/jmspublishconnector/impl/JmsPublishConnectorLogger.java
+++ src/main/java/jmspublishconnector/impl/JmsPublishConnectorLogger.java
@@ -1,9 +1,9 @@
 package jmspublishconnector.impl;
 
 import org.camunda.connect.ConnectorRequestException;
-import org.camunda.connect.impl.ConnectCoreLogger;
+import org.camunda.connect.impl.ConnectLogger;
 
-public class JmsPublishConnectorLogger extends ConnectCoreLogger {
+public class JmsPublishConnectorLogger extends ConnectLogger {
     public ConnectorRequestException jmsUrlRequired(){
         return new ConnectorRequestException(exceptionMessage("001", "JMS url is required."));
     }
