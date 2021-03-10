510c61ffb2c28ea5a34a3eeab334597e3fe8fa37
MichalStefaniuk <michal.stefaniuk@devapo.pl>
2021-03-10 13:53:06 +0100

    AMRO-1 Listner reacts to JSON on queue. Start or continue process regarding JSON contents

--- jms-listner/src/main/java/com/example/demo/jms/TransactionReceiver.java
+++ jms-listner/src/main/java/com/example/demo/jms/TransactionReceiver.java
@@ -1,6 +1,7 @@
 package com.example.demo.jms;
 
 import org.camunda.bpm.engine.RuntimeService;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.annotation.JmsListener;
@@ -20,10 +21,30 @@ public class TransactionReceiver {
     public void receiveMessages(Message message) throws JMSException {
         String convertedMessage =((TextMessage) message).getText();
         System.out.println("Received <" + convertedMessage + ">");
-        runtimeService.createMessageCorrelation(convertedMessage)
-                .processInstanceBusinessKey("key")
-                .correlate();
 
-        //runtimeService.startpr
+        JSONObject jsonObject = new JSONObject(convertedMessage);
+
+        Object BUSINESS_KEY = jsonObject.opt("bkey");
+        Object MESSAGE = jsonObject.opt("msg");
+        Object NEW_BUSINESS_KEY = jsonObject.opt("new_bkey");
+
+        /* IF INSTANCE ID IS KNOWN TRIGGER A MSG IN ACTIVE INSTANCE
+           ELSE TRIGGER A MESSAGE IN AN ENGINE TO START A NEW INSTANCE
+                               WITH A GIVEN MSG AS A BUSINESS KEY */
+        if(MESSAGE != null && !MESSAGE.toString().isEmpty()){
+            if(BUSINESS_KEY != null && !BUSINESS_KEY.toString().isEmpty()){
+                runtimeService.createMessageCorrelation(MESSAGE.toString())
+                        .processInstanceBusinessKey(BUSINESS_KEY.toString())
+                        .correlate();
+            } else {
+                System.out.println("BUSINESS KEY EMPTY");
+                System.out.println(NEW_BUSINESS_KEY.toString());
+                runtimeService.startProcessInstanceByMessage(MESSAGE.toString(), NEW_BUSINESS_KEY.toString());
+            }
+        } else {
+            System.out.println("MESSAGE IS EMPTY");
+        }
     }
 }
+
+//https://docs.camunda.org/javadoc/camunda-bpm-platform/7.3/org/camunda/bpm/engine/RuntimeService.html
