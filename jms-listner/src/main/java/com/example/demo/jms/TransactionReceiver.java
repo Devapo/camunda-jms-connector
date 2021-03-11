0914b0a82fc93ce73b0a5e2b4e26e5463961b47a
MichalStefaniuk <michal.stefaniuk@devapo.pl>
2021-03-11 15:47:22 +0100

    AMRO-1 Listner consumes application prop variables

--- jms-listner/src/main/java/com/example/demo/jms/TransactionReceiver.java
+++ jms-listner/src/main/java/com/example/demo/jms/TransactionReceiver.java
@@ -5,6 +5,7 @@ import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
@@ -17,8 +18,19 @@ import javax.jms.TextMessage;
 @EnableJms
 public class TransactionReceiver {
     private final static Logger LOGGER = LoggerFactory.getLogger(TransactionReceiver.class);
+
     @Autowired
     RuntimeService runtimeService;
+
+    @Value("${json.instanceid}")
+    private String INSTANCE_ID;
+
+    @Value("${json.payload}")
+    private String PAYLOAD;
+
+    public static boolean paramIsNotEmpty(String param){
+        return (param != null && !param.isEmpty());
+    }
 
     @JmsListener(destination = "test")
     public void receiveMessages(Message message) throws JMSException {
@@ -28,29 +40,17 @@ public class TransactionReceiver {
 
         JSONObject jsonObject = new JSONObject(convertedMessage);
 
-        Object PROCESS_INSTANCE_BUSINESS_KEY = jsonObject.opt("bkey");
-        Object NEW_PROCESS_BUSINESS_KEY = jsonObject.opt("new_bkey");
-        Object MESSAGE = jsonObject.opt("msg");
+        Object INSTANCE_ID = jsonObject.opt(this.INSTANCE_ID.replace("'", ""));
+        Object PAYLOAD = jsonObject.opt(this.PAYLOAD.replace("'",""));
 
-        /* IF INSTANCE ID IS KNOWN TRIGGER A MSG IN ACTIVE INSTANCE
-           ELSE TRIGGER A MESSAGE IN AN ENGINE TO START A NEW INSTANCE
-                               WITH A GIVEN MSG AS A BUSINESS KEY */
-
-        if(MESSAGE != null && !MESSAGE.toString().isEmpty()){
-            if(PROCESS_INSTANCE_BUSINESS_KEY != null && !PROCESS_INSTANCE_BUSINESS_KEY.toString().isEmpty()){
-                runtimeService.createMessageCorrelation(MESSAGE.toString())
-                        .processInstanceBusinessKey(PROCESS_INSTANCE_BUSINESS_KEY.toString())
-                        .correlate();
-            } else if(NEW_PROCESS_BUSINESS_KEY != null && !NEW_PROCESS_BUSINESS_KEY.toString().isEmpty()){
-                runtimeService.startProcessInstanceByMessage(MESSAGE.toString(), NEW_PROCESS_BUSINESS_KEY.toString());
-            } else {
-                LOGGER.warn("BUSINESS KEY IS EMPTY!");
-                //System.out.println();
-            }
+        if(paramIsNotEmpty(INSTANCE_ID.toString()) && paramIsNotEmpty(PAYLOAD.toString()))
+        {
+            runtimeService.createMessageCorrelation(PAYLOAD.toString())
+                    .processInstanceBusinessKey(INSTANCE_ID.toString())
+                    .correlate();
         } else {
-            LOGGER.warn("MESSAGE IS EMPTY");
+            LOGGER.warn("EMPTY JSON PARAMETER");
         }
     }
 }
-
 //https://docs.camunda.org/javadoc/camunda-bpm-platform/7.3/org/camunda/bpm/engine/RuntimeService.html
