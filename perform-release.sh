a070bbfefc337c91f2acacdb23e3bf4925de54c9
Maciej Nowicki <maciej.nowicki@devapo.io>
2022-06-17 15:50:43 +0200

    Prepared repository for release

---
+++ perform-release.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -e
+
+VERSION=$1
+TAG_NAME="v${VERSION}"
+
+if [[ ${VERSION} == *SNAPSHOT ]]
+then
+  echo "Release version must not be SNAPSHOT version";
+fi
+
+# Prepare release
+mvn versions:set -DnewVersion="${VERSION}" -DgenerateBackupPoms=false
+mvn clean package
+git add -A
+git commit -m "[${VERSION}] Prepare for release"
+git tag "${TAG_NAME}"
+
+# Perform release
+mvn clean deploy -DskipTests
+git push
+git push origin "${TAG_NAME}"
+
+# Prepare development iteration
+mvn versions:use-next-snapshots
+git add -A
+git commit -m "Prepare for next development iteration"
+git push
